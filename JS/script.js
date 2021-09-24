89b7acab0bc5e0347d8a3cb2fc8fea7617abe9a0
Wosen100 <83084085+Wosen100@users.noreply.github.com>
2021-09-23 20:46:04 -0400

    myfirstbranchcommit

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+// placing two slashes in front of the cond or text you wish to have the java script interpreter ignore.
+
+/* This is how you comment any text between will be ignored by JavaScript. This example uses a multi-line comment (a comment block) to explaine th code:*/
+
